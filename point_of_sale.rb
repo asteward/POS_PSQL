276c49e06d4a16d66044ff28a752e2ab6dcd5b00
Austin Steward & Jeff Stringer <student@epicodus.com>
2014-08-12 12:12:23 -0700

    add create_products

--- point_of_sale.rb
+++ point_of_sale.rb
@@ -9,13 +9,13 @@ development_configuration = database_configurations['development']
 ActiveRecord::Base.establish_connection(development_configuration)
 
 def welcome
-  puts "Welcome to the Point of Sale (POS) system!"
+  puts "\nWelcome to the Point of Sale (POS) system!"
   menu
 end
 
 def menu
   choice = nil
-    puts "Select C to create a new customer"
+    puts "\nSelect C to create a new customer"
     puts "Select L to create logins for new cashiers"
     puts "Select A add a new product"
     puts "Select R to initiate a return"
@@ -25,8 +25,8 @@ def menu
     user_input = gets.chomp
     case user_input
       when 'C' then create_customer
-      when 'L'
-      when 'A'
+      when 'L' then create_cashier
+      when 'A' then create_product
       when 'R'
       when 'T'
       when 'E'
@@ -42,5 +42,23 @@ def create_customer
   puts "#{customer.name} has been added."
 end
 
+def create_cashier
+  puts "Enter cashier name:"
+  print ">"
+  cashier_name = gets.chomp
+  cashier = Cashier.create({name: cashier_name})
+  puts "#{cashier.name} has been added."
+end
+
+def create_product
+  puts "Enter product name:"
+  print ">"
+  product_name = gets.chomp
+  puts "Enter product price:"
+  print ">$"
+  product_price = gets.chomp
+  product = Product.create({name: product_name, price: product_price})
+  puts "#(product.name} with a price of #{product.price} has been added."
+end
 
 welcome
